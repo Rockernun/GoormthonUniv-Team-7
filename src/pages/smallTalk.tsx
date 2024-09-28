aba3b21bc19c3001b6f268897c6317cdc96737d8
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-09-28 15:56:50 +0900

    style : 스몰토크 UI 스타일 수정

--- src/pages/smallTalk.tsx
+++ src/pages/smallTalk.tsx
@@ -1,39 +1,58 @@
+import React, { useState } from 'react';
 import { LargeButton } from 'components/Button/Button';
 import NavBar from 'components/NavBar';
 import Description from 'components/ToolTips/Description';
 
-//  MBTI 부분을 텍스트로 받아와야 함
+//  MBTI 부분을 텍스트로 받아와야 오고, 그에 맞게 Description의 text도 바뀌어야 함.
 const SmallTalk: React.FC = () => {
-  return (
-    <div>
-      <div className="flex justify-center items-center">
-        <div className="flex flex-col w-[375px] h-[860px] p-4 bg-background_color relative">
-          <div className="mt-[30px]">
-            <NavBar subject="cloud" />
-            <div className="flex justify-center items-center h-[450px]">
-              <div className="text-6xl">MBTI</div>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <div className="absolute top-[470px] left-[65px]">
-                <Description text={`MBTI 첫 인상을 맞춰보세요!\n예시 : ENTJ 일거 같아요!`} />
-              </div>
-              <div className="absolute top-[550px] left-[90px]">
-                <img
-                  src="/assets/GoormCharacter.svg"
-                  alt="cloud-character"
-                  className="w-[192px] h-[192px]"
-                />
-              </div>
+  const [currentTopic, setCurrentTopic] = useState(0);
+  const topic = [
+    {
+      subject: 'MBTI',
+      description: 'MBTI에 대해 이야기해보세요!',
+      category: 'small_talk',
+    },
+    {
+      subject: '스포츠',
+      description: '좋아하는 스포츠가 뭐야?',
+      category: 'small_talk',
+    },
+    {
+      subject: '게임',
+      description: '무슨 게임 해?',
+      category: 'small_talk',
+    },
+    {
+      subject: '아이돌',
+      description: '어떤 아이돌 좋아해?',
+      category: 'small_talk',
+    },
+  ];
 
-              <div className="absolute left-[20px] top-[700px] w-[335px]">
-                <LargeButton text="다음 주제" />
-              </div>
-            </div>
-          </div>
+  const handleNextTopic = () => {
+    setCurrentTopic((prev) => (prev + 1) % topic.length);
+  };
+
+  return (
+    <div className="p-[11px]">
+      <NavBar subject="drink" />
+      <div className="flex flex-col justify-center items-center relative">
+        <div className="flex gap-2 justify-center items-center h-[450px] text-txt_secondary">
+          <img src="/assets/cloud.svg" alt="cloud-emoji" className="h-6 w-6" />
+          <div>스몰토크 주제</div>
         </div>
+        <div className="absolute text-6xl top-64">{topic[currentTopic].subject}</div>
+      </div>
+      <div className="absolute flex flex-col justify-center items-center w-[352px] bottom-[44px]">
+        <Description text={topic[currentTopic].description} />
+        <img className="mb-[-45px]" src="/assets/GoormCharacter.svg" alt="goorm-character" />
+        <LargeButton text="다른 게임 할래요" onClick={handleNextTopic} />
       </div>
     </div>
   );
 };
 
 export default SmallTalk;
+
+//  밸런스 게임 선택하도록
+//  퍼센트를 기입함으로써 중복 선택하는 사람들 방지
