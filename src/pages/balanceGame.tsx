7c6e7c338d61470c51fbe3defc501d8681959f98
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-09-28 16:34:31 +0900

    style : 밸런스 게임 UI 예시 구현

--- src/pages/balanceGame.tsx
+++ src/pages/balanceGame.tsx
@@ -6,6 +6,18 @@ import Description from 'components/ToolTips/Description';
 import EtcBox from 'components/box/EtcBox';
 
 const BalanceGame: React.FC = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const questions = [
+    { question_a: '100% 확률 10억', question_b: '50% 확률 1,000억' },
+    { question_a: '무제한 여행', question_b: '무제한 식사' },
+    { question_a: '평생 안 씻어도 깨끗함', question_b: '평생 폭식해도 살 안 찜' },
+    { question_a: '똥맛 카레', question_b: '카레맛 똥' },
+  ];
+
+  const handleNextQuestion = () => {
+    setCurrentQuestion((prev) => (prev + 1) % questions.length);
+  };
   return (
     <div className="flex justify-center items-center">
       <div className="flex flex-col w-[375px] h-[860px] p-4 bg-background_color relative">
